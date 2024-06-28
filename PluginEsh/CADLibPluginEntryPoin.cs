f34e34b53ebc19cf75860b2c702380e169fdb440
Roman <151859525+RemEshmetov@users.noreply.github.com>
2024-06-28 17:55:31 +0300

    First commit

---
+++ PluginEsh/CADLibPluginEntryPoin.cs
@@ -0,0 +1,31 @@
+﻿using CADLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PluginEsh
+{
+    /// <summary>
+    /// Класс точки входа плагина, выполняющий его регистрацию
+    /// </summary>
+    public class CADLibPluginEntryPoint
+
+    {
+        public CADLibPluginEntryPoint() { }
+
+        /// <summary>
+        /// Статический метод регистрации плагин, вызываемый родительским приложением
+        /// </summary>
+        /// <param name="manager">Объект текущего окружения плагина</param>
+        /// <returns>Интерфейс плагина</returns>
+        public static ICADLibPlugin RegisterPlugin(PluginsManager manager)
+        {
+
+            return new Form1();
+
+        }
+    }
+}
---
+++ PluginEsh/Form1.cs
@@ -0,0 +1,44 @@
+﻿using CADLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plugin
+{
+    public partial class Form1 : Form, ICADLibPlugin
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public MenuStrip GetMenu()
+        {
+            return menuStrip1;
+        }
+
+        public ToolStripContainer GetToolbars()
+        {
+            return null;
+        }
+
+        public void TrackInterfaceItems(InterfaceTracker tracker)
+        {
+
+        }
+
+        private void плагинТестToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var form2 = new Form2())
+            {
+                form2.ShowDialog();
+            }
+        }
+    }
+}
